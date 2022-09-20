6495c6359c451ed857f18649acb34c8848610968
tzn <tzn1104@126.com>
2022-09-20 13:01:40 +0800

    init: vue源码学习仓库初始化；基本环境搭建完成

---
+++ packages/reacitivity/dist/reactivity.iife.js
@@ -0,0 +1,8 @@
+var VueReactivity = (() => {
+  // packages/shared/src/index.ts
+  var isObject = (obj) => typeof obj === "object" && obj !== null;
+
+  // packages/reactivity/src/index.ts
+  console.log(isObject);
+})();
+//# sourceMappingURL=reactivity.iife.js.map
